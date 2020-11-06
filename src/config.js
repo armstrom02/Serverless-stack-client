80f4709b4940453c9d1f345af680ba677136a608
armstrom02 <viveksinghreborn@gmail.com>
2020-11-06 18:06:24 +0530

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
         REGION: "us-east-2",
         BUCKET: "notes-app-vivek"
@@ -12,5 +13,6 @@ export default {
         USER_POOL_ID: "us-east-2_KVcpswXsO",
         APP_CLIENT_ID: "74cjvsjj16abpuhkfjpblrb6j4",
         IDENTITY_POOL_ID: "us-east-2:55c5397e-ac46-4705-bd12-17306b6156f4"
-    }
+    },
+    STRIPE_KEY: "pk_test_Dys9puapakNqonoQdfTQj5IL00uEFRuiGk",
 };
