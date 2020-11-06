317da3dbc8499ee7459f14be08c0b3295b3a9022
armstrom02 <viveksinghreborn@gmail.com>
2020-11-06 13:08:18 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-vivek"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://5cmtuejiki.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_KVcpswXsO",
+        APP_CLIENT_ID: "74cjvsjj16abpuhkfjpblrb6j4",
+        IDENTITY_POOL_ID: "us-east-2:55c5397e-ac46-4705-bd12-17306b6156f4"
+    }
+};
